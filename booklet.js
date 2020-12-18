f3de7306ce74dcd911153a9c3e74f708af5a2865
patriksimurka <patriksimurka@gmail.com>
2020-12-18 20:50:56 +0100

    t

--- booklet.js
+++ booklet.js
@@ -1,5 +1,5 @@
 (function() {
-	var regexp = new RegExp(/playerCaptionsTracklistRenderer*?(youtube.com\/api\/timedtext.*?)"/);
+	var regexp = new RegExp(/playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/);
 	var url = regexp.exec(document.body.innerHTML)[1];
 	alert(url);
 })();
